f23451e65a6da988b16fcab2c4ee3563b84a8063
onaolaalbert <onaolaalbert@gmail.com>
2026-03-02 17:56:57 +0000

    Create AppTest.java

---
+++ airport_sim_2/src/test/java/com/airport_sim_2/AppTest.java
@@ -0,0 +1,13 @@
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
