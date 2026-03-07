bcb0824e36563de6f12894739261cfe4fe8adb2e
Xu <xudongchen2006@gmail.com>
2026-03-07 18:50:56 +0000

    added cancellations in take off queue

--- airport_sim_2/src/main/java/com/airport_sim_2/queues/TakeOffQueue.java
+++ airport_sim_2/src/main/java/com/airport_sim_2/queues/TakeOffQueue.java
@@ -1,5 +1,7 @@
 package com.airport_sim_2.queues;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import com.airport_sim_2.objects.Aircraft;
@@ -42,4 +44,33 @@ public class TakeOffQueue implements AircraftQueue {
         
         return callsigns;
     }
+
+    public int checkCancelled(int runways, long take_off_time, long max_wait_time){
+        // Initialise list of cancelled planes and copy the queue
+        List<Aircraft> cancelled = new ArrayList<>();
+        Queue<Aircraft> copy = new LinkedList<>(queue);
+
+        int position = 1;
+
+        while (!copy.isEmpty()){
+            Aircraft aircraft = copy.poll();
+
+            // Calculates the wait times and time the fuel will last 
+            double wait_time = Math.ceil((double) position / runways) * take_off_time;
+
+            if (wait_time > max_wait_time){
+                cancelled.add(aircraft);
+            }
+            // ignore plane
+            else{
+                position++;
+            }
+        }
+
+        //remove all diverted planes from the holding pattern and return the list of diverted planes
+        queue.removeAll(cancelled);
+        //return diverted;
+        return cancelled.size();
+    }
+
 }
