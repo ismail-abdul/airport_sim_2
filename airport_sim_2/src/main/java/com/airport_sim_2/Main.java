5e405dc8df5f4d399223e642ced7948af15ee19a
Liumirai <tonevalidia1@gmail.com>
2026-03-13 00:30:29 +0000

    comitting before working on more stuff

--- airport_sim_2/src/main/java/com/airport_sim_2/Main.java
+++ airport_sim_2/src/main/java/com/airport_sim_2/Main.java
@@ -1,7 +1,9 @@
 package com.airport_sim_2;
+
 import com.airport_sim_2.controller.SimulationController;
 
 public class Main {
+
     public static void main(String[] args) {
         double endtime = 60 * (10 * 10 * 10);
         SimulationController controller = new SimulationController(endtime);
