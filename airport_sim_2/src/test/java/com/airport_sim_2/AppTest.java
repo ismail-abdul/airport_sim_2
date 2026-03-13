57126bec822b8d2d55666de421ba70c5b1a7e854
onaolaalbert <onaolaalbert@gmail.com>
2026-03-13 18:12:11 +0000

    Create AppTest.java

---
+++ airport_sim_2/src/test/java/com/airport_sim_2/AppTest.java
@@ -0,0 +1,20 @@
+package com.airport_sim_2;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Basic environment check for airport_sim_2.
+ * Run 'mvn test' to verify JUnit is working in the new repo.
+ */
+public class AppTest 
+{
+    /**
+     * If this passes when 'mvn test' is run then Maven environment is set up correctly.
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
